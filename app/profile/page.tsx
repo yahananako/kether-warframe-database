a20d8cc10270f984fb838e406f64dda8f4f33775
yahananako <yahananako@gmail.com>
2026-07-03 22:54:56 +0000

    Remove leftover stat frame injected script v2.4.9

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -47,7 +47,7 @@ export default function ProfilePage() {
         <div>
           <p>KETHER OF PARADISO</p>
           <h1>個人進度中心</h1>
-          <span>v2.4.8 個人化資料庫前置架構，準備接入 Discord 與資料庫。</span>
+          <span>v2.4.9 個人化資料庫前置架構，準備接入 Discord 與資料庫。</span>
         </div>
       </header>
 
