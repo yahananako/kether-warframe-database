bde193821f09521d15a5c045f0fa247b44ce176e
yahananako <yahananako@gmail.com>
2026-07-04 13:04:31 +0000

    chore: update developer credit for v2.5.35

--- app/page.tsx
+++ app/page.tsx
@@ -267,7 +267,7 @@ export default async function HomePage() {
 
           <div className="total-row">
             <span>網站版本</span>
-            <b>v2.5.34</b>
+            <b>v2.5.35</b>
           </div>
         </article>
 
@@ -277,7 +277,7 @@ export default async function HomePage() {
             <span>5　備註</span>
           </div>
 
-          <p>・v2.5.34 已加入首頁自動資料總控台。</p>
+          <p>・v2.5.35 已加入首頁自動資料總控台。</p>
           <p>・下一階段可做 Discord 登入前置與個人化資料庫設計。</p>
           <p>・完成度目前讀取表格欄位，未來會改為個人獨立紀錄。</p>
           <div className="note-lines" />
@@ -296,7 +296,7 @@ export default async function HomePage() {
           https://kether-warframe-database.vercel.app
         </a>
         <span className="home-footer-divider">｜</span>
-        <span className="home-footer-designer">Website by ヤハ奈々子</span>
+        <span className="home-footer-designer">Website by ヤハ奈々子・羊咩・凱洛</span>
       </footer>
 
     </main>
